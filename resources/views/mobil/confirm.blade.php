c6e3b752ed33189373b6c4ee88aadf61c0080350
Satriowish <satriowisnuap@gmail.com>
2025-04-19 08:47:48 +0700

    Perbaikan View Confirm Delete pada Tabel Mobil

--- resources/views/mobil/confirm.blade.php
+++ resources/views/mobil/confirm.blade.php
@@ -1,8 +1,8 @@
-@empty($jenis)
+@empty($mobil)
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5>
+                <h5 class="modal-title">Kesalahan</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -12,18 +12,18 @@
                     <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
                     Data yang Anda cari tidak ditemukan
                 </div>
-                <a href="{{ url('/jenis') }}" class="btn btn-warning">Kembali</a>
+                <a href="{{ url('/mobil') }}" class="btn btn-warning">Kembali</a>
             </div>
         </div>
     </div>
 @else
-    <form action="{{ url('/jenis/' . $jenis->id . '/delete') }}" method="POST" id="form-delete">
+    <form action="{{ url('/mobil/' . $mobil->id . '/delete') }}" method="POST" id="form-delete">
         @csrf
         @method('DELETE')
         <div id="modal-master" class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Hapus Data Jenis</h5>
+                    <h5 class="modal-title">Hapus Data Mobil</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -31,21 +31,33 @@
                 <div class="modal-body">
                     <div class="alert alert-warning">
                         <h5><i class="icon fas fa-ban"></i> Konfirmasi !!!</h5>
-                        Apakah Anda ingin menghapus data seperti di bawah ini?
+                        Apakah Anda yakin ingin menghapus data mobil berikut?
                     </div>
                     <table class="table table-sm table-bordered table-striped">
                         <tr>
-                            <th class="text-right col-3">Nama Jenis :</th>
-                            <td class="col-9">{{ $jenis->name }}</td>
+                            <th class="text-right col-3">Nama Mobil:</th>
+                            <td class="col-9">{{ $mobil->name }}</td>
                         </tr>
                         <tr>
-                            <th class="text-right col-3">Tipe Jenis :</th>
-                            <td class="col-9">{{ $jenis->type }}</td>
+                            <th class="text-right col-3">Merk:</th>
+                            <td class="col-9">{{ $mobil->brand }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-right col-3">Tahun:</th>
+                            <td class="col-9">{{ $mobil->year }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-right col-3">Jumlah:</th>
+                            <td class="col-9">{{ $mobil->quantity }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-right col-3">Jenis:</th>
+                            <td class="col-9">{{ $mobil->jenis->name ?? '-' }} - {{ $mobil->jenis->type ?? '-' }}</td>
                         </tr>
                     </table>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
+                    <button type="button" class="btn btn-warning" data-dismiss="modal">Batal</button>
                     <button type="submit" class="btn btn-danger">Ya, Hapus</button>
                 </div>
             </div>
@@ -55,9 +67,8 @@
     <script>
         $(document).ready(function () {
             $("#form-delete").submit(function (e) {
-                e.preventDefault(); // Mencegah form default submission
+                e.preventDefault();
 
-                // Menampilkan SweetAlert untuk konfirmasi
                 Swal.fire({
                     title: "Apakah Anda yakin?",
                     text: "Data yang dihapus tidak dapat dikembalikan!",
@@ -68,43 +79,26 @@
                     confirmButtonText: "Ya, Hapus!"
                 }).then((result) => {
                     if (result.isConfirmed) {
-                        // Mengirimkan permintaan AJAX
                         $.ajax({
                             url: $(this).attr('action'),
                             type: $(this).attr('method'),
                             data: $(this).serialize(),
                             success: function (response) {
-                                // Cek apakah statusnya true
                                 if (response.status) {
-                                    $('#myModal').modal('hide'); // Menutup modal
-                                    Swal.fire({
-                                        icon: 'success',
-                                        title: 'Berhasil',
-                                        text: response.message // Menampilkan pesan sukses
-                                    });
-                                    dataJenis.ajax.reload(); // Menyegarkan data jenis jika menggunakan datatable
+                                    $('#myModal').modal('hide');
+                                    Swal.fire('Berhasil', response.message, 'success');
+                                    dataMobil.ajax.reload();
                                 } else {
-                                    // Jika status false
-                                    Swal.fire({
-                                        icon: 'error',
-                                        title: 'Terjadi Kesalahan',
-                                        text: response.message // Menampilkan pesan kesalahan
-                                    });
+                                    Swal.fire('Gagal', response.message, 'error');
                                 }
                             },
                             error: function () {
-                                // Menampilkan pesan jika AJAX gagal
-                                Swal.fire({
-                                    icon: 'error',
-                                    title: 'Terjadi Kesalahan',
-                                    text: 'Gagal menghapus data'
-                                });
+                                Swal.fire('Error', 'Terjadi kesalahan saat menghapus data.', 'error');
                             }
                         });
                     }
                 });
             });
         });
-
     </script>
 @endempty
